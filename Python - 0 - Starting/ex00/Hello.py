47e163213576aa7c01ec190b1143e2e523f81193
theoapp <theoappourchaux62@gmail.com>
2025-02-10 19:51:03 +0100

    chore: start day one and push all subjects

---
+++ Python - 0 - Starting/ex00/Hello.py
@@ -0,0 +1,12 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "France!")
+ft_set = {"Hello", "Angoulême!"}
+ft_dict = {"Hello": "42Angoulême!"}
+
+def main():
+	print(ft_list)
+	print(ft_tuple)
+	print(ft_set)
+	print(ft_dict)
+
+main()
