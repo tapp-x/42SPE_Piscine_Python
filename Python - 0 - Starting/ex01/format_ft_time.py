fc2c7b09d71fd5b9940d8aab13703e9b503681f3
theoapp <theoappourchaux62@gmail.com>
2025-02-26 16:24:13 +0100

    clean: fixing last flake8 errors

--- Python - 0 - Starting/ex01/format_ft_time.py
+++ Python - 0 - Starting/ex01/format_ft_time.py
@@ -7,5 +7,6 @@ diff = (x - a)
 sec = diff.total_seconds()
 
 
-print("Seconds since January 1, 1970:", f"{sec:,.0f}", "or", format(sec, ".2e"), "in scientific notation")
+print("Seconds since January 1, 1970:\
+", f"{sec:,.0f}", "or", format(sec, ".2e"), "in scientific notation")
 print(x.strftime("%b"), x.strftime("%d"), x.year)
